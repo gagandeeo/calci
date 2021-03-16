ae0d376502171739aebf52227f08bb1f00a92571
gagandeeo <gagandeepprasad9@gmail.com>
2021-03-16 15:55:01 +0530

    updated

--- resources/views/about.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-    <title>Hello ABOUT</title>
-    </head>
-    <body>
-    <h1>HelloworldABOUT!!</h1>
-    </body>
-</html>
